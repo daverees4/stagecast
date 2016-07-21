fa4ce8d308bfc30bbd2bb083c762838da66bb477
David Rees <d.w.rees.03@gmail.com>
2016-07-21 13:47:51 +0200

    JS Scroll

--- js/app.js
+++ js/app.js
@@ -4,21 +4,26 @@ flowplayer(function(api){
     $('#chapter1').on('click', function() {
       api.seek(90.5);
       api.play();
+      $("html, body").animate({ scrollTop: 0 }, "slow");
     });
     $('#chapter2').on('click', function() {
       api.seek(340);
       api.play();
+      $("html, body").animate({ scrollTop: 0 }, "slow");
     });
     $('#chapter3').on('click', function() {
       api.seek(526);
       api.play();
+      $("html, body").animate({ scrollTop: 0 }, "slow");
     });
     $('#chapter4').on('click', function() {
       api.seek(801);
       api.play();
+      $("html, body").animate({ scrollTop: 0 }, "slow");
     });
     $('#chapter5').on('click', function() {
       api.seek(1023);
       api.play();
+      $("html, body").animate({ scrollTop: 0 }, "slow");
     });
 });
